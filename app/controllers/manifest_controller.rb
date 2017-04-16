f7b2851220da2f1dcf072c2512e3243e61db823e
iprojas <iquelo>
2017-04-16 01:43:49 -0300

    👆 Manifiest

--- app/controllers/manifest_controller.rb
+++ app/controllers/manifest_controller.rb
@@ -3,13 +3,13 @@ class ManifestController < ApplicationController
 
   def show
     render json: {
-      name: ENV['SITE_NAME'] || 'Loomio',
-      short_name: ENV['SITE_NAME'] || 'Loomio',
+      name: ENV['SITE_NAME'] || 'Asamblea de Revolución Democrática',
+      short_name: ENV['SITE_NAME'] || 'Asamblea RD',
       display: 'standalone',
       orientation: 'portrait',
       start_url: '/dashboard',
       background_color: '#ffffff',
-      theme_color: '#ffffff',
+      theme_color: '#007769',
       icons: [{
         src:   [root_url.chomp('/'), :img, :"logo-square.png"].join('/'),
         sizes: '144x144',
